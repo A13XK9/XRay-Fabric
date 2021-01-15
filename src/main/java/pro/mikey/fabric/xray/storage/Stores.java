bad92f4fd4be035ea66afaf06b3d60e757cb1108
alex <alexxkrupa@gmail.com>
2021-01-15 11:51:16 -0600

    xray

--- src/main/java/pro/mikey/fabric/xray/storage/Stores.java
+++
@@ -1,6 +0,0 @@
-package pro.mikey.fabric.xray.storage;
-
-public class Stores {
-    public static final SettingsStore SETTINGS = SettingsStore.getInstance();
-    public static final BlockStore BLOCKS = BlockStore.getInstance();
-}
